16072140e0ab04fe0a48fb5f845c3b6f35a5c821
luistrofa2 <luismaia99@gmail.com>
2019-04-26 14:24:08 +0100

    new proj

--- GestVendasAppMVC/src/GestVendas.java
+++ GestVendasAppMVC/src/GestVendas.java
@@ -1,7 +1,24 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Main do programa
- */
 public class GestVendas {
 
+    public static void main(String[] args) {
+        String fich = "Vendas_1M.txt";
+        List<String> linhas = new ArrayList<>();
+        BufferedReader inStream = null;
+        String linha = null;
+        try {
+            inStream = new BufferedReader(new FileReader(fich));
+            while ((linha = inStream.readLine()) != null) {
+                linhas.add(linha);
+            }
+        }
+        catch (IOException e) {
+            System.out.println(e);
+        }
+    }
 }
