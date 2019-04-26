fc4dead8eb44802fb30114107ef3c9f8add7ee11
luistrofa2 <luismaia99@gmail.com>
2019-04-26 17:24:49 +0100

    Working printf vendas

--- GestVendasAppMVC/src/GestVendas.java
+++ GestVendasAppMVC/src/GestVendas.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -6,11 +7,15 @@ import java.util.List;
 
 public class GestVendas {
 
-    public static void main(String[] args) {
-        String fich = "Vendas_1M.txt";
+    public static void main(String[] args) throws Exception{
+        String filePath = "GestVendasAppMVC/Ficheiros/Vendas_1M.txt";
+        File fich = new File(filePath);
+        FileReader fr = new FileReader(fich);
+        //BufferedReader br = new BufferedReader(fr);
+        //String fich = "Vendas_1M.txt";
         List<String> linhas = new ArrayList<>();
-        BufferedReader inStream = null;
-        String linha = null;
+        BufferedReader inStream;
+        String linha;
         try {
             inStream = new BufferedReader(new FileReader(fich));
             while ((linha = inStream.readLine()) != null) {
@@ -20,5 +25,11 @@ public class GestVendas {
         catch (IOException e) {
             System.out.println(e);
         }
+        int i = 0;
+        for (String c : linhas){
+            System.out.println(c+"----->"+"["+i+"]");
+            i++;
+        }
+        System.out.println("ESTA A FUNCIONAR!...");
     }
 }
