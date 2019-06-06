9912ea5b1c832196e4ac0a5298ef6f97eb561ffb
luistrofa2 <luismaia99@gmail.com>
2019-06-06 15:30:18 +0100

    gerevendaview menuqueries

--- GestVendasAppMVC/src/GereVendasView.java
+++ GestVendasAppMVC/src/GereVendasView.java
@@ -7,4 +7,18 @@ public class GereVendasView implements  InterfGereVendasView{
         System.out.println("Produtos invalidos: "+invalidos);
     }
 
+    public void MenuQueries() {
+        System.out.println("Menu Queries\n\nEscolha uma query:\n1.Lista de Produtos nunca comprados.\n" +
+                "2.Dando um mes ou filial, determina vendas e o total de clientes que as fizeram.\n" +
+                "3.Dado um cliente, determina mes a mes, total de compras,produtos distintos e quanto gastou.\n" +
+                "4.Dado um produto, determina mes a mes, total de vezes comprado, clientes distintos, e total faturado.\n" +
+                "5.Dado um cliente, determina os produtos que mais comprou.\n" +
+                "6.Determina, segundo um numero dado, os produtos mais vendidos em todo o ano.\n" +
+                "7.Determina para cada filial, a lista dos 3 maiores compradores.\n" +
+                "8.Determina, segundo um numero dade, os clientes que comprar mais produtos diferentes.\n" +
+                "9.Dado um produto, determina, segundo um numero dado, os clientes que mais compraram e o seu valor gasto.\n" +
+                "10.Determinar mes a mes, e para cada mes, filial a filial, a faturacao total de cada produto.\n");
+    }
+
+
 }
