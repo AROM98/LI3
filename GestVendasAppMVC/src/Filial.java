4a7f20cf74a5e4473a766a84cf5b51148015a327
luistrofa2 <luismaia99@gmail.com>
2019-06-06 14:12:11 +0100

    filiais num array erro

--- GestVendasAppMVC/src/Filial.java
+++ GestVendasAppMVC/src/Filial.java
@@ -7,9 +7,7 @@ public class Filial {
     private CatProd produtos;
 
     //Filiais -> contem registo de compras realizadas em cada filial, por quem e quando.
-    private List<Venda> filial1; //agora contem vendas, mas é provavel que haja alteraçoes.
-    private List<Venda> filial2;
-    private List<Venda> filial3;
+    private List<Venda> filial[]; //agora contem vendas, mas é provavel que haja alteraçoes.
 
     /**
      * Construtores -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -17,24 +15,25 @@ public class Filial {
 
     public Filial(){
         this.produtos = new CatProd();
-        this.filial1 = new ArrayList<Venda>();
-        this.filial2 = new ArrayList<Venda>();
-        this.filial3 = new ArrayList<Venda>();
+        for(int i=0;i<3;i++)
+        this.filial[i] = new ArrayList<Venda>();
+     /*   this.filial2 = new ArrayList<Venda>();
+        this.filial3 = new ArrayList<Venda>();*/
     }
 
     public Filial(CatProd prod, List<Venda> filial1, List<Venda> filial2, List<Venda> filial3){
         this.produtos = prod;
-        this.filial1 = filial1;
-        this.filial2 = filial2;
-        this.filial3 = filial3;
+        this.filial[0] = filial1;
+        this.filial[1] = filial2;
+        this.filial[2] = filial3;
     }
-
+/*
     public Filial(Filial f){
         this.produtos = f.getProdutos();
         this.filial1 = f.getFilial1();
         this.filial2 = f.getFilial2();
         this.filial3 = f.getFilial3();
-    }
+    }*/
 
     /**
      * Gets -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -45,15 +44,15 @@ public class Filial {
     }
 
     public List<Venda> getFilial1() {
-        return filial1;
+        return filial[0];
     }
 
     public List<Venda> getFilial2() {
-        return filial2;
+        return filial[1];
     }
 
     public List<Venda> getFilial3() {
-        return filial3;
+        return filial[2];
     }
 
     /**
@@ -65,25 +64,25 @@ public class Filial {
     }
 
     public void setFilial1(List<Venda> filial1) {
-        this.filial1 = filial1;
+        this.filial[0] = filial1;
     }
 
     public void setFilial2(List<Venda> filial2) {
-        this.filial2 = filial2;
+        this.filial[1] = filial2;
     }
 
     public void setFilial3(List<Venda> filial3) {
-        this.filial3 = filial3;
+        this.filial[2] = filial3;
     }
 
 
     /**
      * Metodos -+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
      */
-
+/*
     public Filial clone(){
         return new Filial(this);
-    }
+    }*/
 
     public boolean equals(Object obj) {
         if(this == obj){
@@ -94,35 +93,37 @@ public class Filial {
         }
         Filial aux = (Filial) obj;
         return this.produtos.equals(aux.getProdutos())
-                && this.filial1.equals(aux.getFilial1())
-                && this.filial2.equals(aux.getFilial2())
-                && this.filial3.equals(aux.getFilial3());
+                && this.filial[0].equals(aux.getFilial1())
+                && this.filial[1].equals(aux.getFilial2())
+                && this.filial[2].equals(aux.getFilial3());
     }
 
     /**
      * vendas sao sepradas para cada array de Filial
      */
     public void preencheFilialais(Venda v){
-        if(v.getFilial() == 1){
-            this.filial1.add(v);
+        /*if(v.getFilial() == 1){
+            this.filial[0].add(v);
         }
         if(v.getFilial() == 2){
             this.filial2.add(v);
         }
         if(v.getFilial() == 3){
             this.filial3.add(v);
-        }
+        }*/
+        this.filial[v.getFilial()-1].add(v);
     }
 
     public void printa(){
-        for(Venda v : filial1){
-            System.out.println(v);
-        }
+        for(int i = 0;i<3;i++)
+            for(Venda v : filial[i]){
+                System.out.println(v);
+        }/*
         for(Venda v : filial2){
             System.out.println(v);
         }
         for(Venda v : filial3){
             System.out.println(v);
-        }
+        }*/
     }
 }
