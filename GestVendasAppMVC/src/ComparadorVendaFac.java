a8bfedad1fc57a1142e42238226af4a594c43150
AROM98 <rafa.olim@sapo.pt>
2019-06-06 19:35:39 +0100

    Comparador de Venda pelo preço

---
+++ GestVendasAppMVC/src/ComparadorVendaFac.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+
+public class ComparadorVendaFac implements Comparator<Venda> {
+
+    public int compare(Venda v1, Venda v2){
+
+        if(v1.getPreco() <= v2.getPreco())
+            return 1;
+        if(v1.getPreco() > v2.getPreco())
+            return -1;
+        return 0;
+    }
+}
